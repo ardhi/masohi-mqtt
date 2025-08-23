f9e851bb6a947e6a82d36cfdc9f67637629fd592
Ardhi Lukianto <ardhi@lukianto.com>
2025-08-23 23:41:12 +0700

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 async function factory (pkgName) {
   const me = this
 
-  return class MasohiMqtt extends this.lib.BajoPlugin {
+  return class MasohiMqtt extends this.lib.Plugin {
     constructor () {
       super(pkgName, me.app)
       this.alias = 'mqtt'
